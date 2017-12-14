74e467e5195f8689b272f6e5550a6779350c24bd
David Farrenkopf <dfarrenk@gmail.com>
2017-12-14 17:18:14 +0000

    file structure layout finished

--- config/connection.js
+++ config/connection.js
@@ -16,29 +16,6 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    // afterConnection();
 });
 
-
-// // View the table without having to keep reinitializing myphp
-// var Table = require('cli-table');
-
-// function afterConnection() {
-//     //Print burger table.
-//     connection.query("SELECT * FROM burgers", function(err, res) {
-//         if (err) throw err;
-//         // instantiate
-//         var table = new Table({
-//             head: ['id', 'burger_name', 'devoured', 'date'],
-//             // colWidths: [50, 50]
-//         });
-
-//         // table is an Array, so you can `push`, `unshift`, `splice` and friends
-//         for (let i in res) {
-//             table.push(
-//                 [res[i].id, res[i].burger_name, res[i].devoured, res[i].date]
-//             );
-//         }
-//         console.log(table.toString());
-//     });
-// }
+module.exports = connection;
--- config/orm.js
+++ config/orm.js
@@ -1,19 +1,56 @@
-var connection = require('connection.js');
+// Import MySQL
+var connection = require('../config/connection.js');
 
-//   * In the `orm.js` file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
+// Helper function for SQL syntax.
+// Let's say we want to pass 3 values into the mySQL query.
+// In order to write the query, we need 3 question marks.
+// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
+// ["?", "?", "?"].toString() => "?,?,?";
+// function printQuestionMarks(num) {
+//     var arr = [];
 
-//      * `selectAll()`
-//      * `insertOne()`
-//      * `updateOne()`
+//     for (var i = 0; i < num; i++) {
+//         arr.push("?");
+//     }
 
-//   * Export the ORM object in `module.exports`.
+//     return arr.toString();
+// }
+
+// // Helper function to convert object key/value pairs to SQL syntax
+// function objToSql(ob) {
+//     var arr = [];
+
+//     // loop through the keys and push the key/value as a string int arr
+//     for (var key in ob) {
+//         var value = ob[key];
+//         // check to skip hidden properties
+//         if (Object.hasOwnProperty.call(ob, key)) {
+//             // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+//             if (typeof value === "string" && value.indexOf(" ") >= 0) {
+//                 value = "'" + value + "'";
+//             }
+//             // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+//             // e.g. {sleepy: true} => ["sleepy=true"]
+//             arr.push(key + "=" + value);
+//         }
+//     }
+
+//     // translate array of strings to a single comma-separated string
+//     return arr.toString();
+// }
 
 var orm = {
-    selectall: function() {
-
+    selectall: function(callback) {
+        connection.query("Select * FROM burgers", function(error, response) {
+            if (error) throw error;
+            callback(response);
+        });
     },
-    insertOne: function() {
-
+    insertOne: function(burgerName, callback) {
+        connection.query("INSERT INTO burgers (burger_name) VALUES (?)", [burgerName], function(error, response) {
+            if (error) throw error;
+            callback(response);
+        });
     },
     updateOne: function() {
 
@@ -21,3 +58,32 @@ var orm = {
 };
 
 module.exports = orm;
+
+
+// View the table without having to keep reinitializing myphp
+var Table = require('cli-table');
+
+function afterConnection(query) {
+    //Print burger table.
+    // console.log(query);
+    connection.query("SELECT * FROM burgers", function(err, res) {
+        if (err) throw err;
+        // instantiate
+        var table = new Table({
+            head: ['id', 'burger_name', 'devoured', 'date'],
+            // colWidths: [50, 50]
+        });
+        for (let i in res) {
+            table.push(
+                [res[i].id, res[i].burger_name, res[i].devoured, res[i].date]
+            );
+        }
+        console.log(table.toString());
+
+
+        // table is an Array, so you can `push`, `unshift`, `splice` and friends
+    });
+}
+
+// orm.selectall(afterConnection);
+orm.insertOne("The Camelot", afterConnection);
