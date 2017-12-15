b9149d353db281e521679abd21b59fe7e334f848
David Farrenkopf <dfarrenk@gmail.com>
2017-12-15 17:42:14 +0000

    JawsDB added

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,26 @@
 var mysql = require("mysql");
 
 //Database connection
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
+var connection
 
-    // Your username
-    user: "root",
+if (process.env.JAWSDB_URL) {
+    //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
 
-    // Your password
-    password: "",
-    database: "burger_db"
-});
+        host: "localhost",
+        port: 3306,
 
+        // Your username
+        user: "root",
+
+        // Your password
+        password: "",
+        database: "burger_db"
+    });
+}
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
